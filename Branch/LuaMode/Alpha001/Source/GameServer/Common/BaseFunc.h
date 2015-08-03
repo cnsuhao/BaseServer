2dc14709fd9a5130ca6b8662afee3ae702055a05
c913421043 <c913421043@b7d7a4be-286e-455e-978f-175bf5e4c3c4>
2015-08-03 10:07:46 +0000

    * 整理基础函数类

--- Branch/LuaMode/Alpha001/Source/GameServer/Common/BaseFunc.h
+++ Branch/LuaMode/Alpha001/Source/GameServer/Common/BaseFunc.h
@@ -17,6 +17,8 @@
 #include <vector>
 using namespace std;
 
+//////////////////////////////////////////////////////////////////////////
+// 全局变量声明
 extern char g_szServerName[64];				// 服务器名
 extern int  g_nServerLine;					// 服务器线路号
 extern int  g_nServerGroup;					// 服务器组号
@@ -25,18 +27,79 @@ extern std::string g_strConstDatabaseName;	// 常量表数据库名
 extern std::string g_strGameDatabaseName;	// 游戏服数据库名
 extern std::string g_strAccountDatabaseName;// 账号服数据库名
 
+//////////////////////////////////////////////////////////////////////////
+// 时间函数相关
+enum TIME_TYPE	// 时间类型枚举
+{
+	TIME_BEGIN,
+	TIME_MILLISECOND=TIME_BEGIN,	// 毫秒					::timeGetTime()
+	TIME_SECOND,					// 秒					::time(NULL)
+	TIME_MINUTE,					// 分					YYMMDDHHMM
+	TIME_HOUR,						// 时					YYMMDDHH
+	TIME_DAY,						// 天					YYMMDD
+	TIME_DAYSECOND,					// 每天的时间 精确到秒  HHMMSS
+	TIME_DAYMIN,					// 每天的时间 精确到分  HHMM
+	TIME_DAYHOUR,					// 每天的时间 精确到时  HH
+	TIME_DAY_START,					// 今天的第一秒
+	TIME_DAY_END,					// 今天最后一秒
+	TIME_WEEK_DAY,					// 本周第几天			
+	TIME_WEEK_START,				// 本周第一秒
+	TIME_WEEK_END,					// 本周最后一秒
+	TIME_MONTH_DAY,					// 本月第几号
+	TIME_MONTH_START,				// 本月第一秒
+	TIME_MONTH_END,					// 本月最后一秒
+	TIME_YEAR_DAY,					// 年中第几天
+	TIME_YEAR_MONTH_DAY,			// 年月日				YYYYMMDD
+	TIME_STAMP,						// 时间戳				MMDDHHMMSS
+	TIME_EPOCH_DAY,					// 时间点.当前时间距离参考时间点的天数
+	TIME_DAYMINISECOND,				// 拼接天、小时、分钟、秒、毫秒	DDHHMMSSss
+	TIME_END,
+};
+DWORD	TimeGet(TIME_TYPE type = TIME_MILLISECOND);
+int     GetUTCSecond(int nTimeType, I64 i64Time);
+I64		GetUtcMillisecond(void);
+UINT	GetUtcTimeConvert(UINT unTimeSecond, TIME_TYPE eType);
+
+UINT	GetNextWeekStartUtcTime(void);
+UINT    GetNextUpdateTime(UINT unHour, TIME_TYPE type = TIME_SECOND);
 UINT	GetNextUpdateTimeOfWeek(UINT unTimeOfDay);
-String	DumpBuffer(const char* buf, int nSize);
-void	DateTime(char * buf20, time_t tInput = 0);		// 0 : curr time
-bool	FormatDateTime(char* buf, const char* szFormat, time_t tInput = 0);		// szFormat: "%04d-%02d-%02d %02d:%02d:%02d"
-void	DateTimeStamp(char * buf15, time_t tInput = 0);		// 0 : curr time
-int		DateStamp(time_t tTime=0);
-int		DateStampPass(int nDate, int nDays);	// return -1: error
+UINT	GetNextUpdateMonthDate(void);
+
+bool	IsTimeOver(UINT nEndDate);
+int		GetHourUTCSecond(int nHour);
+int		GetTimeSpanFromSecond(UINT unUtcTime);
+int     GetTimeSpanToMunite(UINT nDataTime);
+int		GetTimeSpanToDay(DWORD dwTimeDay);
+UINT    GetTimeMinuteFromLeftMunite(int nMuniteOffset);
+
+void	DateTime(char * buf20, time_t tInput = 0);
+bool	FormatDateTime(char* buf, const char* szFormat, time_t tInput = 0);
+int		DateStamp(time_t tTime = 0);
+void	DateTimeStamp(char * buf15, time_t tInput = 0);
+int		DateStampPass(int nDate, int nDays);
 bool	IsActiveTime(time_t tCurr, unsigned long nFormat);
 bool	IsTimeRange(unsigned long nBegin, unsigned long nEnd, time_t tCurr=0);
-char*	StrStrCh(const char* string, const char* strCharSet);
-int		CalcNumberAxisIntersection(int x1, int y1, int x2, int y2);
+//////////////////////////////////////////////////////////////////////////
 
+//////////////////////////////////////////////////////////////////////////
+// 数值函数相关
+template<TYPENAME T>
+inline T	CutOverflow(T x, T y) { return (x <= y) ? x : y; }
+template<TYPENAME T>
+inline T	CutTrail(T x, T y) { return (x >= y) ? x : y; }
+template<TYPENAME T>
+inline T	CutRange(T n, T min, T max) { return (n<min) ? min : ((n>max) ? max : n); }
+
+// 将值限定在合理范围内
+template <class T>
+T GetValueInRightRange(T value, T minValue, T maxValue)
+{
+	value = min(value, maxValue);
+	value = max(value, minValue);
+	return value;
+}
+
+// 检测值是否在合理范围，不合理就输出默认值
 template<class T>
 bool CheckBetween(IN OUT T& nowVaule, T minVaule, T maxVaule, T defaultValue)
 {
@@ -53,122 +116,50 @@ bool CheckBetween(IN OUT T& nowVaule, T minVaule, T maxVaule, T defaultValue)
 	return bResult;
 }
 
-///枚请在后面增加 脚本要用到举
-enum TIME_TYPE{
-	TIME_BEGIN,
-	TIME_MILLISECOND=TIME_BEGIN, 
-	TIME_SECOND, 
-	TIME_MINUTE, 
-	TIME_HOUR,
-	TIME_DAY, 
-	TIME_DAYTIME, 
-	TIME_STAMP,
-	TIME_EPOCH_DAY,//20070402:时间点.当前时间距离参考时间点的天数
-	MONTH_DAY,//20070402:得到当月的几号
-	YEAR_DAY,//20070402:得到一年中的第几天
-	WEEK_DAY,//得到一个星期的第几天
-	YEAR_MONTH_DAY,
-	TIME_DAYHOUR,//每天的时间 精确到小时
-	TIME_DAYMIN,//每天的时间 精确到分钟
-	TIME_DAY_START,
-	TIME_DAY_END,
-	TIME_WEEK_START,
-	TIME_WEEK_END,
-	TIME_MONTH_START,
-	TIME_MONTH_END,
-	TIME_END,
-};
+int		RandGet(int nMax, bool bReset=false);
+int		RandGetZT(int nMax, double dRangeParamL = 3.0, double dRangeParamR = 3.0, bool bRest = false);
+double	RandomRateGet(double dRange);
 
-UINT	UtcSecondConvert(UINT unSecond, TIME_TYPE eType);
-DWORD	TimeGet(TIME_TYPE type=TIME_MILLISECOND);
-I64		GetUtcMillisecond(void);
-int		RandGet		(int nMax, bool bReset=false);
-int		RandGetZT(int nMax, double dRangeParamL=3.0, double dRangeParamR=3.0, bool bRest=false);
-void	DDALineEx(int x0, int y0, int x1, int y1, vector<POINT>& vctPoint);
-//获得一个天、小时、分钟、秒、毫秒拼接在数字
-DWORD GetTimeDayToMiniSecond();
+int		Double2Int(double dValue);
+double  DoubleSaveTwoDecimal(double dInput);
 
-UINT GetNextUpdateTime(UINT unHour, TIME_TYPE type = TIME_SECOND);
-UINT GetLog2(UINT nNum);
+int		bit_count(int n);
+int		bit_i64_count(I64 i64Data, int nEndIndex = 64);
 
-// 获得当前时间与参数的间隔时间，支持TIME_MINUTE
-UINT GetTimeDuring(UINT unLastTime);
-
-bool IsTimeOver(UINT nEndDate);
-int GetCurTimeSpan(UINT nDataTime);
-int GetNextWeekStartUTC();
-int GetYearMonthDayFormUtcSecond(time_t nUtcSecond);
-
-//[游途道标 2009.06.10]资产流向日志
+UINT	GetLog2(UINT nNum);
 //////////////////////////////////////////////////////////////////////////
-enum
-{
-	NONE = 0,
-		BOOTH_SALE,//摆摊卖
-		BOOTH_BUY,//摆摊购买
-		BANGPAI_HONGBAO,//发红包
-		TRADE_ONE,//交易1
-		TRADE_TWO,//交易2
-		SHOP_ADDBUY,//添加寄售信息
-		SHOP_SALE,//店铺购买物品
-		SHOP_ADDSALE,//添加出售物品
-		SHOP_BUY,//店铺收购
-		SHOP_DEL_SALE,//退回出售物品
-		SHOP_DEL_BUY,//退回收购物品
-		SHOP_PAIMAI_TUIHUI,//退回拍卖钱
-		SHOP_PAIMAI,//拍卖
-		SHOP_XUZU,//店铺续租
-		SHOP_TIQU,//店铺提取
-		MAIL_YIBAN,//邮件日志
-		MAIL_JIAOYI,//邮件日志
-		MAIL_TUIHUI,//邮件日志
-};
 
-struct STPROPERTYSTREAMLOG
-{
-	DWORD	idSource;//源标识
-	DWORD	idSAccount;
-	DWORD	dwSVas;//源当前VAS
-	DWORD	dwSMoney;//源当前游戏币
-	DWORD	idTarget;//目标标识
-	DWORD	idTAccount;
-	DWORD	dwTVas;//源当前VAS
-	DWORD	dwTMoney;//源当前游戏币
-	DWORD	dwVas;//流动VAS
-	DWORD	dwMoney;//流动货币
-	DWORD	idItemType;
-	DWORD	idSItem;//流动物品标识
-	DWORD	dwSNumber;//流动物品标识
-	DWORD	idTItem;//流动物品标识
-	DWORD	dwTNumber;//流动物品标识
-	DWORD	dwSysVas;
-	DWORD	dwSysMoney;
-	DWORD	dwType;//类型
-	
-	STPROPERTYSTREAMLOG()
-	{
-		idSource = NULL;//源标识
-		idSAccount = NULL;
-		dwSVas = NULL;//源当前VAS
-		dwSMoney = NULL;//源当前游戏币
-		idTarget = NULL;//目标标识
-		idTAccount = NULL;
-		dwTVas = NULL;//源当前VAS
-		dwTMoney = NULL;//源当前游戏币
-		dwVas = NULL;//流动VAS
-		dwMoney = NULL;//流动货币
-		idItemType = NULL;
-		idSItem = NULL;//流动物品标识
-		dwSNumber = NULL;//流动物品标识
-		idTItem = NULL;//流动物品标识
-		dwTNumber = NULL;//流动物品标识
-		dwSysVas = NULL;
-		dwSysMoney = NULL;
-		dwType = NULL;//类型
-	}
-};
+//////////////////////////////////////////////////////////////////////////
+// 字符串整理函数相关
+String	DumpBuffer(const char* buf, int nSize);
+char*	StrStrCh(const char* string, const char* strCharSet);
 
-void	PropertyStreamLog(STPROPERTYSTREAMLOG &stProStream);
+BOOL	StringCheck	(const char* pszString, BOOL bNewLineEnable=false);
+BOOL	TalkStrCheck(char* pszString, BOOL bNewLineEnable=false);
+BOOL	DisableWordCheck(const char* pszString, BOOL bNewLineEnable=false);
+bool	StrSqlCheck(const char* pszString);
+BOOL	NameStrCheck(const char* pszString);
+
+bool	MyANSIToUnicode(IN const char* pszStr,		OUT wchar_t* pwszStr,	int nUnicodeStrSize);
+bool	MyUnicodeToANSI(IN const wchar_t* pwszStr,	OUT char* pszStr,		int nAnsiStrSize);
+bool	MyUnicodeToUTF8(IN const wchar_t* pwszStr,  OUT char* pszStr,		int nUtf8StrSize);
+bool	MyUTF8ToUnicode(IN const char* pszStr,		OUT wchar_t* pwszStr,	int nUnicodeStrSize);
+
+CMxyString MyUtf8ToANSI(const char* pszString);
+CMxyString MyANSIToUtf8(const char* pszString);
+
+bool	SafeCopy(char * pTarget, const char * pSource, int nBufLen = 0);
+int		ReplaceStdString(IN OUT string& str, const char* pszOld, const char* pszNew, bool bReplaceAll = true);
+//////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////
+// 其他一些函数
+bool	IsExistFile(const char* pszFileName);
+std::string LoadFileToString(const std::string& strFileName);
+
+int		CalcNumberAxisIntersection(int x1, int y1, int x2, int y2);
+
+void	DDALineEx(int x0, int y0, int x1, int y1, vector<POINT>& vctPoint);
 
 //////////////////////////////////////////////////////////////////////////
 //20071212:进制转换.nConvNum:2~9.
@@ -177,15 +168,6 @@ void	PropertyStreamLog(STPROPERTYSTREAMLOG &stProStream);
 //nTrainPoint = 7
 //返回值为11.
 int NumConversion(int nTrainPoint,int nConvNum);
-
-BOOL	StringCheck	(const char* pszString, BOOL bNewLineEnable=false);
-BOOL	TalkStrCheck(char* pszString, BOOL bNewLineEnable=false);
-BOOL	DisableWordCheck(const char* pszString, BOOL bNewLineEnable=false);
-bool	StrSqlCheck(const char* pszString);
-BOOL	NameStrCheck(const char* pszString);
-
-double	RandomRateGet	(double dRange);
-int		Double2Int		(double dValue);
 
 inline int	AddToTop(int nData, int nAdd, int nTop) { int res=nData+nAdd; if(nAdd>0 && res<nData || res>nTop) return nTop; else return res; }
 
@@ -224,14 +206,6 @@ private:
 };
 
 /////////////////////////////////////////////////////////////////////////////
-template<TYPENAME T>
-inline T	CutOverflow(T x, T y) { return (x <= y) ? x : y; }
-template<TYPENAME T>
-inline T	CutTrail(T x, T y) { return (x >= y) ? x : y; }
-template<TYPENAME T>
-inline T	CutRange(T n, T min, T max) { return (n<min) ? min : ((n>max) ? max : n); }
-
-/////////////////////////////////////////////////////////////////////////////
 inline unsigned int	ExchangeShortBits(unsigned long nData, int nBits)
 {
 	ASSERT(nBits >= 0 && nBits < 16);
@@ -263,49 +237,10 @@ inline void possub(POINT* pTarget, int nOffsetX, int nOffsetY) { pTarget->x -= n
 inline bool ConvertMailText(OUT LPSTR lpstrText, UINT nAction, int nParam1 = 0, int nParam2 = 0, int nParam3 = 0, 
 							int nParam4 = 0, int nParam5 = 0, int nParam6 = 0, LPCSTR lpstrParam1 = "", LPCSTR lpstrParam2 = "");
 
-bool MyANSIToUnicode(IN const char* pszStr,		OUT wchar_t* pwszStr,	int nUnicodeStrSize);
-bool MyUnicodeToANSI(IN const wchar_t* pwszStr,	OUT char* pszStr,		int nAnsiStrSize);
-bool MyUnicodeToUTF8(IN const wchar_t* pwszStr, OUT char* pszStr,		int nUtf8StrSize);
-bool MyUTF8ToUnicode(IN const char* pszStr,		OUT wchar_t* pwszStr,	int nUnicodeStrSize);
-
-CMxyString MyUtf8ToANSI(const char* pszString);
-CMxyString MyANSIToUtf8(const char* pszString);
-
-int bit_count(int n);//检测二进制中的1的个数
-int I64_bit_count(I64 I64Data, int nEndIndex = 64);//检测二进制中的1的个数
-UINT GetNextUpdateMonthDate();//用于月掩码清理，获取下个月的每一天(YYYYMMDD)精确到日
-
-UINT GetItemLifeTimeMinute(int nLifeTimeOffset);
-
 bool IsRightAuthenID(DWORD dwAuthenID, DWORD dwAccount);				// 验证登陆时的票id
-bool SafeCopy(char * pTarget, const char * pSource, int nBufLen = 0);	// 字符串安全拷贝
 bool IsObjType(OBJID idObjType, OBJID idUnion);							// 判断对象type
-double SaveTwoDecimal(double dInput);									// double类型保留两位小数
-DWORD GetPassDay(DWORD dwTimeDay);
-
-////////////////////////////////////////////////////////////////////////
-// Description:  将值限定在合理范围内
-// Arguments:
-// Author: 彭文奇(Peng Wenqi)
-// Return Value: class T
-////////////////////////////////////////////////////////////////////////
-template <class T>
-T GetValueInRightRange(T value, T minValue, T maxValue)
-{
-	value = min(value, maxValue);
-	value = max(value, minValue);
-	return value;
-}
 
 LONG WINAPI MyUnhandledExceptionFilter(struct _EXCEPTION_POINTERS* ExceptionInfo);
-
-int GetUTCSecond(int nTimeType, I64 i64Time);
-int GetHourUTCSecond(int nHour);
-int GetTimeSpanFromDayBeginOfUTC(UINT unUTCTime);
-
-std::string LoadFileToString(const std::string& strFileName);	//从文件读入到string里
-int ReplaceStdString(IN OUT string& str, const char* pszOld, const char* pszNew, bool bReplaceAll = true);	// 替换std::string中的字符
-bool IsExistFile(const char* pszFileName);
 
 std::string GetLocalIpAddr();
 
